e4c9f8dcdaf5a0fc12526f1d4f98fe7978b7b0f6
Rob Grant <rob.grant@nanoporetech.com>
2022-12-01 10:06:51 +0000

    Removed unnecessary sorted() call
    Signed-off-by: Rob Grant <rob.grant@nanoporetech.com>

--- day1/run.py
+++ day1/run.py
@@ -5,4 +5,4 @@ from inputs import REAL #, TEST
 elves = sorted([sum(map(int, re.split('\n', elf))) for elf in re.split('\n\n+', REAL)])
 
 print(f"Part 1: {elves[-1]}")
-print(f"Part 2: {sum(sorted(elves)[-3:])}")
+print(f"Part 2: {sum(elves[-3:])}")
