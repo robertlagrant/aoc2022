3903c831e3498082590617e6b719ded0cb24ae4c
Rob Grant <rob.grant@nanoporetech.com>
2022-12-01 10:04:47 +0000

    Day 1
    Signed-off-by: Rob Grant <rob.grant@nanoporetech.com>

---
+++ day1/run.py
@@ -0,0 +1,8 @@
+import re
+
+from inputs import REAL #, TEST
+
+elves = sorted([sum(map(int, re.split('\n', elf))) for elf in re.split('\n\n+', REAL)])
+
+print(f"Part 1: {elves[-1]}")
+print(f"Part 2: {sum(sorted(elves)[-3:])}")
