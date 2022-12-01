0ca84ee09ac5693dd88a4c5c354e148d402459d4
Rob Grant <rob.grant@nanoporetech.com>
2022-12-01 10:08:00 +0000

    Removed random brackets
    Signed-off-by: Rob Grant <rob.grant@nanoporetech.com>

--- day1/run.py
+++ day1/run.py
@@ -2,7 +2,7 @@ import re
 
 from inputs import REAL #, TEST
 
-elves = sorted([sum(map(int, re.split('\n', elf))) for elf in re.split('\n\n+', REAL)])
+elves = sorted(sum(map(int, re.split('\n', elf))) for elf in re.split('\n\n+', REAL))
 
 print(f"Part 1: {elves[-1]}")
 print(f"Part 2: {sum(elves[-3:])}")
