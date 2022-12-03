21c77444a1328effdc239cca769bb8d2b6b52b82
Rob Grant <rob.grant@nanoporetech.com>
2022-12-03 16:18:18 +0000

    Day 3 in 13 crazy lines
    Signed-off-by: Rob Grant <rob.grant@nanoporetech.com>

--- day3/run.py
+++ day3/run.py
@@ -1,22 +1,13 @@
+from functools import reduce
 import string
 
 import inputs
 
 PRIORITIES = {l: i+1 for i, l in enumerate(string.ascii_lowercase + string.ascii_uppercase)}
 
-total_1 = 0
-total_2 = 0
+def common_score(*ins):
+  return PRIORITIES[next(iter(reduce(lambda x, y: set(x) & set(y), ins)))]
 
 lines = inputs.REAL.split('\n')
-
-for line in lines:
-  half = len(line) // 2
-  common = next(iter(set(line[:half]) & set(line[half:])))
-  total_1 += PRIORITIES[common]
-
-for i in range(0, len(lines), 3):
-  common = next(iter(set(lines[i]) & set(lines[i+1]) & set(lines[i+2])))
-  total_2 += PRIORITIES[common]
-
-print(f"Part 1: {total_1}")
-print(f"Part 1: {total_2}")
+print(f"Part 1: {sum(common_score(line[:(halfway := len(line)//2)], line[halfway:]) for line in lines)}")
+print(f"Part 2: {sum(common_score(*lines[i:i+3]) for i in range(0, len(lines), 3))}")
