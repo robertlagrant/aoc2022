ca543f6b66b84e1b7d2659a4f79f20ba4885002e
Rob Grant <rob.grant@nanoporetech.com>
2022-12-04 15:34:40 +0000

    Now with regex
    Signed-off-by: Rob Grant <rob.grant@nanoporetech.com>

--- day4/run.py
+++ day4/run.py
@@ -1,19 +1,15 @@
+import re
+
 import inputs
+
+MATCH = re.compile(r"(\d+)-(\d+),(\d+)-(\d+)")
 
 full_overlap_count = 0
 partial_overlap_count = 0
 
-for lhs, rhs in [line.split(",") for line in inputs.REAL.split("\n")]:
-  l_low, l_high = map(int, lhs.split("-"))
-  r_low, r_high = map(int, rhs.split("-"))
-
-  if l_low <= r_low and r_high <= l_high or \
-     r_low <= l_low and l_high <= r_high:
-    full_overlap_count += 1
-
-  if l_low <= r_low <= l_high or \
-     r_low <= l_low <= r_high:
-    partial_overlap_count += 1
+for l_low, l_high, r_low, r_high in [map(int, re.match(MATCH, line).groups()) for line in inputs.REAL.split("\n")]:
+  full_overlap_count += l_low <= r_low and r_high <= l_high or r_low <= l_low and l_high <= r_high
+  partial_overlap_count += l_low <= r_low <= l_high or r_low <= l_low <= r_high
 
 print(f"Part 1: {full_overlap_count}")
 print(f"Part 2: {partial_overlap_count}")
