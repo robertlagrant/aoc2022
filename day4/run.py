94d876664487759c9236f58ae1b68f2a9ee3b6b1
Rob Grant <rob.grant@nanoporetech.com>
2022-12-04 17:47:18 +0000

    Brevity is the soul of wit
    Signed-off-by: Rob Grant <rob.grant@nanoporetech.com>

--- day4/run.py
+++ day4/run.py
@@ -4,7 +4,7 @@ import inputs
 
 MATCH = re.compile(r"(\d+)-(\d+),(\d+)-(\d+)")
 
-nums = [(ll, lh, rl, rh) for ll, lh, rl, rh in [map(int, re.match(MATCH, l).groups()) for l in inputs.REAL.split("\n")]]
+nums = [list(map(int, re.match(MATCH, line).groups())) for line in inputs.REAL.split("\n")]
 
 print(f"Part 1: {sum(ll <= rl and rh <= lh or rl <= ll and lh <= rh for ll, lh, rl, rh in nums)}")
 print(f"Part 2: {sum(ll <= rl <= lh or rl <= ll <= rh for ll, lh, rl, rh in nums)}")
