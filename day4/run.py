1c6b5340cc4e8dbe5776ee423945a737b4b9ccf8
Rob Grant <rob.grant@nanoporetech.com>
2022-12-04 17:44:38 +0000

    More tidyup
    Signed-off-by: Rob Grant <rob.grant@nanoporetech.com>

--- day4/run.py
+++ day4/run.py
@@ -4,13 +4,8 @@ import inputs
 
 MATCH = re.compile(r"(\d+)-(\d+),(\d+)-(\d+)")
 
-full_overlap_count = 0
-partial_overlap_count = 0
+nums = [(ll, lh, rl, rh) for ll, lh, rl, rh in [map(int, re.match(MATCH, l).groups()) for l in inputs.REAL.split("\n")]]
 
-for l_low, l_high, r_low, r_high in [map(int, re.match(MATCH, line).groups()) for line in inputs.REAL.split("\n")]:
-  full_overlap_count += l_low <= r_low and r_high <= l_high or r_low <= l_low and l_high <= r_high
-  partial_overlap_count += l_low <= r_low <= l_high or r_low <= l_low <= r_high
-
-print(f"Part 1: {full_overlap_count}")
-print(f"Part 2: {partial_overlap_count}")
+print(f"Part 1: {sum(ll <= rl and rh <= lh or rl <= ll and lh <= rh for ll, lh, rl, rh in nums)}")
+print(f"Part 2: {sum(ll <= rl <= lh or rl <= ll <= rh for ll, lh, rl, rh in nums)}")
 
