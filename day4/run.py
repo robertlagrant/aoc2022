1fb7ada785df252d7a52fb85333ce11d512a1035
Rob Grant <rob.grant@nanoporetech.com>
2022-12-04 14:11:53 +0000

    Neater code for day4
    Signed-off-by: Rob Grant <rob.grant@nanoporetech.com>

--- day4/run.py
+++ day4/run.py
@@ -3,15 +3,16 @@ import inputs
 full_overlap_count = 0
 partial_overlap_count = 0
 
-for line in inputs.REAL.split("\n"):
-  lhs, rhs = line.split(",")
+for lhs, rhs in [line.split(",") for line in inputs.REAL.split("\n")]:
   l_low, l_high = map(int, lhs.split("-"))
   r_low, r_high = map(int, rhs.split("-"))
 
-  if l_low <= r_low and l_high >= r_high or r_low <= l_low and r_high >= l_high:
+  if l_low <= r_low and r_high <= l_high or \
+     r_low <= l_low and l_high <= r_high:
     full_overlap_count += 1
 
-  if l_low <= r_low <= l_high or r_low <= l_low <= r_high:
+  if l_low <= r_low <= l_high or \
+     r_low <= l_low <= r_high:
     partial_overlap_count += 1
 
 print(f"Part 1: {full_overlap_count}")
