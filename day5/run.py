3131c475254ecaf11480d7df2ea017a75004e1df
Rob Grant <rob.grant@nanoporetech.com>
2022-12-05 10:58:42 +0000

    Day 5 about to refactor
    Signed-off-by: Rob Grant <rob.grant@nanoporetech.com>

--- day5/run.py
+++ day5/run.py
@@ -1,28 +1,28 @@
 from collections import defaultdict
 import re
 
-HEADER_PATTERN = r"(\d)"
+HEAD_PATTERN = r"(\d)"
 BUCKET_PATTERN = r"([A-Z])"
 INSTRUCTION_PATTERN = r"move (\d+) from (\d) to (\d)"
 
 with open("input.txt") as f:
-    lines = f.readlines()
-    index_matches = re.finditer(HEADER_PATTERN, lines[0])
+    head, rest = f.read().split("\n", 1)
+    bucket_lines, move_lines = rest.split("\n\n")
 
-index_to_bucket = {i: lines[0][i] for i in [m.start(0) for m in index_matches]}
+index_to_bucket = {i: head[i] for i in [m.start(0) for m in re.finditer(HEAD_PATTERN, head)]}
 part_1_columns = defaultdict(list)
 part_2_columns = defaultdict(list)
 
-for line in lines[1:]:
+for line in bucket_lines.split("\n"):
     for index in map(lambda m: m.start(0), re.finditer(BUCKET_PATTERN, line)):
         part_1_columns[index_to_bucket[index]].append(line[index])
         part_2_columns[index_to_bucket[index]].append(line[index])
 
-    if m := re.search(INSTRUCTION_PATTERN, line):
-        count, source, dest = m.groups()
-        for i in range(int(count)):
-            part_1_columns[dest].insert(0, part_1_columns[source].pop(0))
-            part_2_columns[dest].insert(0, part_2_columns[source].pop(int(count)-1-i))
+for line in move_lines.split("\n"):
+    count, source, dest = re.search(INSTRUCTION_PATTERN, line).groups()
+    for i in range(int(count)):
+        part_1_columns[dest].insert(0, part_1_columns[source].pop(0))
+        part_2_columns[dest].insert(0, part_2_columns[source].pop(int(count)-1-i))
 
 print(f"Part 1: {''.join(v[0] for _, v in sorted(part_1_columns.items()))}")
 print(f"Part 2: {''.join(v[0] for _, v in sorted(part_2_columns.items()))}")
