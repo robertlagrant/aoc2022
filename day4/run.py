0440263a1a80a15ae9ddc33b3d499820c89bb95d
Rob Grant <rob.grant@nanoporetech.com>
2022-12-05 10:47:54 +0000

    Davidism-style string split for day 4
    Signed-off-by: Rob Grant <rob.grant@nanoporetech.com>

--- day4/run.py
+++ day4/run.py
@@ -1,9 +1,7 @@
 import re
 
-PATTERN = re.compile(r"(\d+)-(\d+),(\d+)-(\d+)")
-
 with open("input.txt") as f:
-    nums = [list(map(int, m.groups())) for m in map(PATTERN.match, f.readlines())]
+    nums = [list(map(int, re.split("[,-]", l))) for l in f.readlines()]
 
 print(f"Part 1: {sum(l1 <= r1 and r2 <= l2 or r1 <= l1 and l2 <= r2 for l1, l2, r1, r2 in nums)}")
 print(f"Part 2: {sum(l1 <= r1 <= l2 or r1 <= l1 <= r2 for l1, l2, r1, r2 in nums)}")
