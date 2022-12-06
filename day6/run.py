fde5e0e88ffd38b8ea0967a66e0e331113b1e3b5
Rob Grant <rob.grant@nanoporetech.com>
2022-12-06 09:10:06 +0000

    Fixed off by 1 error (that wasn't hit by the examples)
    Signed-off-by: Rob Grant <rob.grant@nanoporetech.com>

--- day6/run.py
+++ day6/run.py
@@ -1,10 +1,10 @@
-import inputs
+from inputs import REAL as signal
 
 def locate_start_packet(s, start_length):
-    for i in range(len(s) - start_length - 1):
+    for i in range(1 + len(s) - start_length):
         if len(set(s[i:i + start_length])) == start_length:
             return i + start_length
 
-print(f"Part 1: {locate_start_packet(inputs.REAL, 4)}")
-print(f"Part 2: {locate_start_packet(inputs.REAL, 14)}")
+print(f"Part 1: {locate_start_packet(signal, 4)}")
+print(f"Part 2: {locate_start_packet(signal, 14)}")
 
