d0afd691d77c6b2ea5d0412a56d21f4ed9c736a9
Rob Grant <rob.grant@nanoporetech.com>
2022-12-06 09:25:14 +0000

    Fun with generators
    Signed-off-by: Rob Grant <rob.grant@nanoporetech.com>

--- day6/run.py
+++ day6/run.py
@@ -1,10 +1,8 @@
 from inputs import REAL as signal
 
-def locate_start_packet(s, start_length):
-    for i in range(1 + len(s) - start_length):
-        if len(set(s[i:i + start_length])) == start_length:
-            return i + start_length
+def locate_unique_sequence(s, length):
+    return (j for i, j in enumerate(range(length, len(s))) if len(set(s[i:j])) == length)
 
-print(f"Part 1: {locate_start_packet(signal, 4)}")
-print(f"Part 2: {locate_start_packet(signal, 14)}")
+print(f"Part 1: {next(locate_unique_sequence(signal, 4))}")
+print(f"Part 2: {next(locate_unique_sequence(signal, 14))}")
 
