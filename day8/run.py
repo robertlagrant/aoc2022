d2b557f4017552781082786882455bd92ddf81ff
Rob Grant <rob.grant@nanoporetech.com>
2022-12-08 11:52:37 +0000

    Target to reduce day8
    Signed-off-by: Rob Grant <rob.grant@nanoporetech.com>

--- day8/run.py
+++ day8/run.py
@@ -1,20 +1,21 @@
 from functools import reduce
-from itertools import takewhile
+from itertools import permutations, takewhile
 from operator import mul
 
 from inputs import REAL as tree_heights
 
 trees = [list(map(int, t_row)) for t_row in tree_heights.split("\n")]
-width = height = len(trees)
+side = len(trees)
 
-taller_trees = {
-  (x, y): {
-    "l": [trees[y][x] <= trees[y][x1] for x1 in range(x-1, -1, -1)],
-    "r": [trees[y][x] <= trees[y][x1] for x1 in range(x+1, width)],
-    "u": [trees[y][x] <= trees[y1][x] for y1 in range(y-1, -1, -1)],
-    "d": [trees[y][x] <= trees[y1][x] for y1 in range(y+1, height)]
-  } for x in range(width) for y in range(height)
-}
+low_trees = [
+    (
+        [trees[y][x] > trees[y][x1] for x1 in range(x - 1, -1, -1)],
+        [trees[y][x] > trees[y][x1] for x1 in range(x + 1, side)],
+        [trees[y][x] > trees[y1][x] for y1 in range(y - 1, -1, -1)],
+        [trees[y][x] > trees[y1][x] for y1 in range(y + 1, side)],
+    )
+    for x, y in permutations(range(side), 2)
+]
 
-print(f"Part 1: {sum(not all(any(taller_trees[(x,y)][d]) for d in 'lrud') for x in range(width) for y in range(height))}")
-print(f"Part 2: {max(reduce(mul, ((taken := sum(not n for n in takewhile(lambda x: not x, tt_d))) + (taken < len(tt_d)) for tt_d in [taller_trees[(x, y)][d] for d in 'lrud'])) for x in range(width) for y in range(height))}")
+print(f"Part 1: {sum(not all(not all(d) for d in t) for t in low_trees)}")
+print(f"Part 2: {max(reduce(mul, ((taken := sum(takewhile(bool, d))) + (taken < len(d)) for d in t)) for t in low_trees)}")
