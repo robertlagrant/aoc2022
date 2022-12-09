97195e6aae0143f7934289d8e54a10f6e7b09ae4
Rob Grant <rob.grant@nanoporetech.com>
2022-12-09 16:24:32 +0000

    Day 9 working
    Signed-off-by: Rob Grant <rob.grant@nanoporetech.com>

--- day9/run.py
+++ day9/run.py
@@ -1,6 +1,7 @@
 from inputs import REAL as commands
 
-directions = {
+S = (0, 0)
+DIRECTIONS = {
     "R": (1, 0), 
     "L": (-1, 0), 
     "U": (0, 1), 
@@ -10,10 +11,8 @@ directions = {
 def steps(commands):
     for d, m in map(lambda s: s.split(), commands):
         for _ in range(int(m)):
-            yield directions[d]
+            yield DIRECTIONS[d]
 
-
-S = (0, 0)
 
 class Knot:
     def __init__(self, name, parent=None):
@@ -25,7 +24,7 @@ class Knot:
         self.pos = [S]
 
 
-    def go(self, x_step=None, y_step=None):
+    def swaaaay(self, x_step=None, y_step=None):
         if self.parent is None:
             self.pos.append((self.pos[-1][0] + x_step, self.pos[-1][1] + y_step))
         else:
@@ -39,32 +38,47 @@ class Knot:
                 self.pos.append(self.pos[-1])
             
         if self.child:
-            self.child.go()
+            self.child.swaaaay()
+
+
+def simulate_tail_positions(knots, commands):
+    prev = root = Knot(name=knots[0])
+    for c in knots[1:]:
+        prev = Knot(name=c, parent=prev)
+
+    for x_step, y_step in steps(commands.split("\n")):
+        root.swaaaay(x_step, y_step)
+    
+    return prev.pos
+
+print(f"Part 1: {len(set(simulate_tail_positions('HT', commands)))}")
+print(f"Part 1: {len(set(simulate_tail_positions('H123456789', commands)))}")
+
+# all_knots = [root]
+# current = root
+# while current.child:
+#     all_knots.append(current.child)
+#     current = current.child
+
+# all_x = []
+# all_y = []
+# for k in all_knots:
+#     for p in k.pos:
+#         # print(k.name, p)
+#         all_x.append(p[0])
+#         all_y.append(p[1])
         
-    def __repr__(self):
-        return f"{self.name} {self.pos} {self.parent.name if self.parent else 'no parent'}"
 
-prev = root = Knot(name="H")
-for c in "T":
-    prev = Knot(name=c, parent=prev)
-
-for i, (x_step, y_step) in enumerate(steps(commands.split("\n"))):
-    root.go(x_step, y_step)
-
-# x_min_print = min(S[0], *[h[0] for h in root.pos], *[t[0] for t in prev.pos])
-# y_min_print = min(S[1], *[h[1] for h in root.pos], *[t[1] for t in prev.pos])
-# x_max_print = max(S[0], *[h[0] for h in root.pos], *[t[0] for t in prev.pos])
-# y_max_print = max(S[1], *[h[1] for h in root.pos], *[t[1] for t in prev.pos])
+# x_min_print, x_max_print = min(all_x), max(all_x)
+# y_min_print, y_max_print = min(all_y), max(all_y)
 
 # for i in range(len(root.pos)):    
-#     # print(f"({i})")
+#     print(f"({i})")
 #     for y in range(y_max_print, y_min_print - 1, -1):
 #         row = ""
 #         for x in range(x_min_print, x_max_print + 1):
-#             if (x, y) == root.pos[i]:
-#                 row += "H"
-#             elif (x, y) == prev.pos[i]:
-#                 row += "T"
+#             if matches := [k.name for k in all_knots if (x, y) == k.pos[i]]:
+#                 row += matches[0]
 #             elif (x, y) == S:
 #                 row += "s"
 #             else:
@@ -72,6 +86,3 @@ for i, (x_step, y_step) in enumerate(steps(commands.split("\n"))):
 #         print(row)
     
 #     print()
-
-print(f"Part 1: {len(set(prev.pos))}")
-
