2bca3da03687f835bccbc84758077f12c9c92d4c
Rob Grant <rob.grant@nanoporetech.com>
2022-12-10 03:11:22 +0000

    Day 9 shorter again
    Signed-off-by: Rob Grant <rob.grant@nanoporetech.com>

--- day9/run.py
+++ day9/run.py
@@ -4,18 +4,16 @@ D = {"R": (1, 0), "L": (-1, 0), "U": (0, 1), "D": (0, -1)}
 
 
 class Knot:
-    def __init__(self, name, parent=None):
-        self.name = name
-        self.child = None
-        if parent:
-            parent.child = self
+    def __init__(self, knots):
+        self.name = knots[0]
         self.pos_history = [(0, 0)]
+        self.child = Knot(knots[1:]) if knots[1:] else None
 
     @property
     def pos(self):
         return self.pos_history[-1]
-    
-    def step(self, s=(0,0)):
+
+    def step(self, s=(0, 0)):
         self.pos_history.append((self.pos[0] + s[0], self.pos[1] + s[1]))
 
     def swaay(self, step=None, parent=None):
@@ -24,17 +22,13 @@ class Knot:
         else:
             ds = parent.pos[0] - self.pos[0], parent.pos[1] - self.pos[1]
             self.step([0 if all(abs(d) <= 1 for d in ds) else -1 if d < 0 else 1 if d > 0 else 0 for d in ds])
-        
+
         return [self.pos] + (self.child.swaay(parent=self) if self.child else [])
 
 
 def simulate_tail_positions(knots, commands):
-    last = first = Knot(name=knots[0])
-    for k in knots[1:]:
-        last = Knot(name=k, parent=last)
-
-    return [first.swaay(D[d])[-1] for d in [d for d, m in (s.split() for s in commands) for _ in range(int(m))]]
+    return [knots.swaay(D[d])[-1] for d in [d for d, m in (s.split() for s in commands) for _ in range(int(m))]]
 
 
-print(f"Part 1: {len(set(simulate_tail_positions('HT', commands.splitlines())))}")
-print(f"Part 2: {len(set(simulate_tail_positions('H123456789', commands.splitlines())))}")
+print(f"Part 1: {len(set(simulate_tail_positions(Knot('HT'), commands.splitlines())))}")
+print(f"Part 2: {len(set(simulate_tail_positions(Knot('H123456789'), commands.splitlines())))}")
