7a45da3beb44ed658b39a3ba1cd262cf9791f646
Rob Grant <rob.grant@nanoporetech.com>
2022-12-11 15:33:39 +0000

    Day 11 save 2 lines for nothing
    Signed-off-by: Rob Grant <rob.grant@nanoporetech.com>

--- day11/run.py
+++ day11/run.py
@@ -7,18 +7,16 @@ from inputs import REAL as DATA
 
 class Monkey:
     inspection_count = 0
-    monkey_parser = re.compile(
-        r"""Monkey \d:
+    monkey_parser = r"""Monkey \d:
   Starting items: (?P<items>[\d, ]+)
   Operation: new = old (?P<op>[\+-/\*]) (?P<op_num>\d+|old)
   Test: divisible by (?P<test>\d+)
     If true: throw to monkey (?P<passed>\d)
     If false: throw to monkey (?P<failed>\d)"""
-    )
     ops = {"+": op.add, "-": op.sub, "*": op.mul, "/": op.floordiv}
 
     def __init__(self, monkey_string):
-        fields = self.monkey_parser.match(monkey_string).groupdict()
+        fields = re.match(self.monkey_parser, monkey_string).groupdict()
         self.items = list(map(int, fields["items"].split(", ")))
         self.op = self.ops[fields["op"]]
         self.op_num = int(fields["op_num"]) if fields["op_num"] != "old" else None  # None means use "old"
