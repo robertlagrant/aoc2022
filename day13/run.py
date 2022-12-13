462b8f7fddb263ca036d42ae71d607789b36ba63
Rob Grant <rob.grant@nanoporetech.com>
2022-12-13 09:59:24 +0000

    Day 13 part 2 getting there
    Signed-off-by: Rob Grant <rob.grant@nanoporetech.com>

--- day13/run.py
+++ day13/run.py
@@ -1,10 +1,13 @@
-from inputs import REAL as data
+from functools import cmp_to_key
+from itertools import chain
+
+from inputs import TEST as data
 
 
 def compare(left, right):
     match([left, right]):
         case [int(), int()]:
-            return 1 if left < right else -1 if right < left else 0
+            return -1 if left < right else 1 if right < left else 0
         case [int(), list()]:
             return compare([left], right)
         case [list(), int()]:
@@ -15,7 +18,11 @@ def compare(left, right):
                 if result != 0:
                     return result
             
-            return 1 if len(left) < len(right) else -1 if len(right) < len(left) else 0
+            return -1 if len(left) < len(right) else 1 if len(right) < len(left) else 0
 
 
-print("Part 1:", sum(i + 1 for i, (l, r) in enumerate([map(eval, pair.split('\n')) for pair in data.split('\n\n')]) if compare(l, r) in (0, 1)))
+pairs = [list(map(eval, pair.split('\n'))) for pair in data.split('\n\n')]
+print("Part 1:", sum(i + 1 for i, (l, r) in enumerate(pairs) if compare(l, r) in (0, -1)))
+
+
+print(sorted([x for x in chain.from_iterable(pairs)], key=cmp_to_key(compare)))
