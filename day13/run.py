4956297dd2dc1a33bfa39af368786861dbeaba10
Rob Grant <rob.grant@nanoporetech.com>
2022-12-13 09:48:29 +0000

    Day 13 part 1 cleaned up
    Signed-off-by: Rob Grant <rob.grant@nanoporetech.com>

--- day13/run.py
+++ day13/run.py
@@ -1,43 +1,21 @@
 from inputs import REAL as data
 
 
-def compare(left, right, num, depth=0):
-    print(" "*depth*2 + f"- Compare {left} vs {right}")
+def compare(left, right):
     match([left, right]):
         case [int(), int()]:
-            if left < right:
-                print(" "*(1+depth)*2 + f"- Left side is smaller, so inputs are in the right order")
-                return 1
-            elif right < left:
-                print(" "*(1+depth)*2 + f"- Right side is smaller, so inputs are not in the right order")
-                return -1
-            
-            return 0
+            return 1 if left < right else -1 if right < left else 0
         case [int(), list()]:
-            print(" "*(1+depth)*2 + f"- Mixed types; convert left to [{left}] and retry comparison")
-            return compare([left], right, num, depth+1)
+            return compare([left], right)
         case [list(), int()]:
-            print(" "*(1+depth)*2 + f"- Mixed types; convert right to [{right}] and retry comparison")
-            return compare(left, [right], num, depth+1)
+            return compare(left, [right])
         case [list(), list()]:
             for l, r in zip(left, right):
-                result = compare(l, r, num, depth+1)
+                result = compare(l, r)
                 if result != 0:
                     return result
             
-            if len(left) < len(right):
-                print(" "*(1+depth)*2 + f"- Left side ran out of items, so inputs are in the right order")
-                return 1
-            elif len(left) == len(right):
-                return 0 
-            
-            print(" "*(1+depth)*2 + f"- Right side ran out of items, so inputs are not in the right order")
-            return -1
-
-for i, (left, right) in enumerate([map(eval, pair.split("\n")) for pair in data.split("\n\n")]):
-    print(f"== Pair {i+1} ==")
-    
-    print(compare(left, right, i))
+            return 1 if len(left) < len(right) else -1 if len(right) < len(left) else 0
 
 
-print("Part 1:", sum(i + 1 for i, (l, r) in enumerate([map(eval, pair.split('\n')) for pair in data.split('\n\n')]) if compare(l, r, i) in (0, 1)))
+print("Part 1:", sum(i + 1 for i, (l, r) in enumerate([map(eval, pair.split('\n')) for pair in data.split('\n\n')]) if compare(l, r) in (0, 1)))
